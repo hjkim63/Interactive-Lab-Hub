b86b715f000e28bad934c0f75c7ca34fd477ab1c
hjkim63 <89815458+hjkim63@users.noreply.github.com>
2022-09-20 02:56:45 -0400

    update

--- Lab 2/lab2i.py
+++ Lab 2/lab2i.py
@@ -2,6 +2,7 @@ import digitalio
 import board
 import time
 from time import strftime, sleep
+from datetime import datetime, timedelta
 import subprocess
 
 from adafruit_rgb_display.rgb import color565
@@ -74,49 +75,44 @@ buttonB = digitalio.DigitalInOut(board.D24)
 buttonA.switch_to_input()
 buttonB.switch_to_input()
 
+#now = datetime.now()
+stretch_time = 0
+a = 0
 
 while True:
-    # Draw a black filled box to clear the image.
+
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    # cmd = "hostname -I | cut -d' ' -f1"
     greeting_1 = "Sitting down?"
-    greeting_2 = "Press me!"
-    start_1 = "It's now " + str(strftime("%H:%M:%S"))
-    start_2 = "I'll light up"
-    start_3 = "when it's time to stetch"
-        
+    greeting_2 = "< Press me!"
 
     # Write four lines of text.
     y = top
-    draw.text((x, y), greeting, font=font, fill="#FFFFFF")
+    draw.text((x, y), greeting_1, font=font, fill="#FFFFFF")
     y += font.getsize(greeting_1)[1]
     draw.text((x, y), greeting_2, font=font, fill="#FFFF00")
     y += font.getsize(greeting_2)[1]
-    # draw.text((x, y), greeting_3, font=font, fill="#0000FF")
-    # y += font.getsize(greeting_3)[1]
-    # draw.text((x, y), Temp, font=font, fill="#FF00FF")
 
     # Display image.
     disp.image(image, rotation)
     time.sleep(0.1)
 
-# while True:
-#     print (strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True)
-#     print("\r", end="", flush=True)
-#     sleep(1)
 
 
-# Main loop:
-while True:
-    if buttonA.value and buttonB.value:
-        backlight.value = False  # turn off backlight
-    else:
-        backlight.value = True  # turn on backlight
     if buttonB.value and not buttonA.value:  # just button A pressed
-        curr_time = strftime("%m/%d/%Y %H:%M:%S")
+        a += 1
         
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+
+        curr_time = strftime("%H:%M:%S")
+        stretch_time = datetime.strptime(curr_time, "%H:%M:%S") + timedelta(minutes=1)
+        
+        stretch_time = format(stretch_time, '%H:%M:%S')
+
+        start_1 = "It's now " + str(strftime("%H:%M:%S"))
+        start_2 = "I'll light up when"
+        start_3 = "it's time to stetch"
+        stretch_ = str(stretch_time)
 
         y = top
         draw.text((x, y), start_1, font=font, fill="#FFFFFF")
@@ -125,9 +121,48 @@ while True:
         y += font.getsize(start_2)[1]
         draw.text((x, y), start_3, font=font, fill="#0000FF")
         y += font.getsize(start_3)[1]
+        draw.text((x, y), stretch_, font=font, fill="#0000FF")
+        y += font.getsize(stretch_)[1]
 
-        # display.fill(screenColor) # set the screen to the users color
-    if buttonA.value and not buttonB.value:  # just button B pressed
-        display.fill(color565(255, 255, 255))  # set the screen to white
-    if not buttonA.value and not buttonB.value:  # none pressed
-        display.fill(color565(0, 255, 0))  # green
+        # Display image.
+        disp.image(image, rotation)
+        time.sleep(10)
+    
+    #sit_start = strftime("%H:%M:%S")
+    #stretch_time = datetime.strptime(sit_start, "%H:%M:%S")  + timedelta(seconds=2)
+    #stretch_time = stretch_time.time()
+   
+    # display.fill(screenColor) # set the screen to the users color
+    # if buttonA.value and not buttonB.value:  # just button B pressed
+        # disp.fill(color565(255, 255, 255))  # set the screen to white
+        # display.fill(color565(0, 255, 0))  # green
+
+        # Display image.
+        # disp.image(image, rotation)
+        # time.sleep(0.1)       
+
+    if a >=1:
+        
+        stretch_time = datetime.strptime(stretch_, "%H:%M:%S")
+        #print("now_min",str(now.minute))
+        #print("stretch_time", str(stretch_time.minute))
+
+        if datetime.now().hour == stretch_time.hour and datetime.now().minute == stretch_time.minute:
+            
+            draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+
+            stretch_1 = "Let's stretch!"
+            stretch_2 = "Busy right now?"
+            stretch_3 = "Snooze for a hot sec!"
+
+            y = top
+            draw.text((x, y), stretch_1, font=font, fill="#FFFFFF")    
+            y += font.getsize(stretch_1)[1]
+            draw.text((x, y), stretch_2, font=font, fill="#FFFF00")
+            y += font.getsize(stretch_2)[1]
+            draw.text((x, y), stretch_3, font=font, fill="#0000FF")
+            y += font.getsize(stretch_3)[1]
+
+            # Display image.
+            disp.image(image, rotation)
+            time.sleep(30)   
